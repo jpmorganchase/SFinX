51464246d2b6c46cf65ba18f6229cb2a4ef33ed1
Russ Kociuba <russell.kociuba@jpmchase.com>
2022-04-27 17:49:59 -0400

    Fixed test case conditions

--- tests/fintypes/test_period.py
+++ tests/fintypes/test_period.py
@@ -1,5 +1,6 @@
 from tests.globals import sample_fin_table
 from sfinx.fintypes.period import FinTabPeriod
+from datetime import datetime
 
 
 def test_period():
@@ -7,8 +8,10 @@ def test_period():
     period, non_headers = FinTabPeriod.get_period({}, "sheet", cell)
     assert period is not None
     assert period.expr.strip() == '2019'
-    assert period.start_date.day == 23
-    assert period.end_date.day == 24
+
+    # The date parser will use the current month/day if it only parses a date
+    assert period.start_date.day == datetime.now().day
+    assert period.end_date.day == datetime.now().day + 1
     assert len(non_headers) == 4
     assert non_headers[0].val == 'FY 20182'
     assert non_headers[1].val == 'Business performance, $MM'
